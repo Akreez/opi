839f28059aaeab5998e75135ef47f816563ea9cc
farkas_tamas <farkast2@gmail.com>
2025-02-25 09:10:49 +0100

    Kezdés

---
+++ src/Engineer.java
@@ -0,0 +1,11 @@
+public class Engineer extends Employee{
+    String diploma;
+    public Engineer(String name){
+        this.name = name;
+        this.diploma = "ismeretlen";
+    }
+
+    public Engineer(){
+        this.diploma = "ismeretlen";
+    }
+}
